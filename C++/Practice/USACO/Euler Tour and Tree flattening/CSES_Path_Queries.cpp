ee538d4b67aab643f505042a0752857fcfb3d43e
N-Crypt3d <89503458+N-Crypt3d@users.noreply.github.com>
2023-07-01 14:54:09 +0600

    usaco update

--- C++/Practice/USACO/Euler Tour and Tree flattening/CSES_Path_Queries.cpp
+++ C++/Practice/USACO/Euler Tour and Tree flattening/CSES_Path_Queries.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define int long long
 
 const int N = 200005;
-vector<int> edge[N], tour, st(N, 0), subtreesize(N, 1), dis(N, 0), w(N);
+vector<int> edge[N], tour, st(N, 0), subtreesize(N, 1), dis(N, 0), w(N), par(N, 0);
 vector<bool> vis(N, 0);
 
 void dfs(int u) {
@@ -17,50 +17,58 @@ void dfs(int u) {
 	for (int v : edge[u]) {
 		if (!vis[v]) {
 			dis[v] += dis[u];
+			par[v] = u;
 			dfs(v);
 			subtreesize[u] += subtreesize[v];
 		}
 	}
 }
+//Don't forget to clear the global variables
+//lo -- hi = range of the index
+// i -- j = range of query
+
+long long a[N];
+struct info {
+	long long prop, sum;
+} tree[N * 4];
 
 
-long long a[N], tree[N * 4];
-
-
-void init (int node , int lo, int hi) {
+void init(int node, int lo, int hi) {
 	if (lo == hi) {
-		tree[node] = a[lo];
+		tree[node].sum = a[lo];
 		return;
 	}
-	int left = node * 2 , right = left + 1;
-	int mid = (lo + hi) / 2 ;
-	init (left, lo, mid);
-	init (right, mid + 1, hi);
-	tree[node] = tree[left] + tree[right];
+	int left = 2 * node, right = left + 1;
+	int mid = (lo + hi) / 2;
+	init(left, lo, mid);
+	init(right, mid + 1, hi);
+	tree[node].sum = tree[left].sum + tree[right].sum;
 }
 
-long long query(int node, int lo, int hi, int i, int j) {
+long long query(int node, int lo, int hi, int i, int j, long long carry = 0LL) {
 	if (i > hi || j < lo) return 0;
-	if (lo >= i && hi <= j) return tree[node];
+	if (i <= lo && j >= hi)return tree[node].sum + carry * (hi - lo + 1) * 1LL;
 
-	int left = node * 2 , right = left + 1;
+	int left = 2 * node, right = left + 1;
 	int mid = (lo + hi) / 2;
-	return query (left, lo, mid, i, j) + query (right, mid + 1, hi, i, j);
+
+	return query(left, lo, mid, i , j , carry + tree[node].prop) + query(right, mid + 1, hi, i , j , carry + tree[node].prop);
 }
 
-void update(int node, int lo, int hi, int i, long long new_value) {
-	if (i < lo || i > hi) return;
-	if (lo == hi) {
-		tree[node] = new_value;
+void update(int node, int lo, int hi, int i, int j, long long val) {
+	if (i > hi || j < lo) return;
+	if (i <= lo && j >= hi) {
+		tree[node].sum += 1LL * (hi - lo + 1) * val;
+		tree[node].prop += val;
 		return;
 	}
-	int left = node * 2, right = left + 1;
-	int mid = (lo + hi) / 2;
-	update(left, lo, mid, i, new_value);
-	update(right, mid + 1, hi, i, new_value);
-	tree[node] = tree[left] + tree[right];
-}
 
+	int left = 2 * node, right = left + 1;
+	int mid = (lo + hi) / 2;
+	update(left, lo, mid, i, j, val);
+	update(right, mid + 1, hi, i, j, val);
+	tree[node].sum = tree[left].sum + tree[right].sum + (hi - lo + 1) * tree[node].prop * 1LL;
+}
 
 
 int32_t main() {
@@ -74,12 +82,9 @@ int32_t main() {
 		edge[u].push_back(v);
 		edge[v].push_back(u);
 	}
+	par[1] = 0;
 	dfs(1);
-	for (int i = 0; i < tour.size(); i++) {
-		// cout << tour[i] << ' ';
-		cout << dis[tour[i]] << ' ';
-		a[i] = dis[tour[i]];
-	}
+	for (int i = 0; i < tour.size(); i++) a[i] = dis[tour[i]];
 	init(1, 0, n - 1);
 	while (q--) {
 		int t;
@@ -87,11 +92,12 @@ int32_t main() {
 		if (t == 1) {
 			int u, val;
 			cin >> u >> val;
-			update(1, 0, n - 1, st[u] - 1, val - (val - dis[u]));
+			int cur = query(1, 0, n - 1, st[u] - 1, st[u] - 1) - query(1, 0, n - 1, st[par[u]] - 1, st[par[u]] - 1);  // getting the current distance of path from root. distance[u] - distance[parent[u]] from the updated seg tree
+			update(1, 0, n - 1, st[u] - 1, st[u] + subtreesize[u] - 2, val - cur);  // increasing the change in seg tree.
 		} else {
 			int u;
 			cin >> u;
-			int ans = query(1, 0, n - 1, st[u] - 1, st[u] + subtreesize[u] - 2);
+			int ans = query(1, 0, n - 1, st[u] - 1, st[u] - 1);
 			cout << ans << '\n';
 		}
 	}
